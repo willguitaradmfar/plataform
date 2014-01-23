f0aa6a3de1d5a8bc8cd7101765d13ed5c6937b1f
William Lima <willguitaradmfar@gmail.com>
2014-01-23 07:27:13 -0500

    Codigo Jquery do menu atualizado

--- admin/public/jomow/js/custom.js
+++ admin/public/jomow/js/custom.js
@@ -1,69 +1,37 @@
-
 //to declarando a posição da pagina
-var home = 0;
-var site = 595;
-var loja = 988;
-var contato = 1381;
+var site = $("#site").offset().top;
+var loja = $("#loja").offset().top;
+var contato = $("#contato").offset().top;
+
 
 $( document ).scroll(function() {
             
          var a = $(document).scrollTop();
         
-         $('.site').removeClass("active2");
-         $('li').removeClass("active");
-         $('.loja').removeClass("active3");
-         $('.contato').removeClass("active4");
-         
-         if(a < site){
-             $('.home').addClass("active");
-               }else 
-         
-         if(a >= site && a < loja){
-             $('.site').addClass("active2");
-               }else 
-         
-         if(a >= loja && a < contato){
-                 $('.site').removeClass("active2");
-            $('.loja').addClass("active3");
-         }else
-         if(a >= contato){
-               $('.loja').removeClass("active3");
-             $('.contato').addClass("active4");
+         $('.site,li,.loja,.contato').removeClass("active2").removeClass("active").removeClass("active3").removeClass("active4");
+       
+         if(a < site){$('.home').addClass("active");}else
+         if(a >= site && a < loja){$('.site').addClass("active2");}else
+         if(a >= loja && a < contato){$('.site').removeClass("active2");$('.loja').addClass("active3");}else
+         if(a >= contato){$('.loja').removeClass("active3");$('.contato').addClass("active4");
            
          }
 });
 
 
+$('a.menu').click(function(){
+      
+    var bob = $(this).attr('href');
+    var local = $(bob).offset().top;
+    
+    $('html, body').animate({scrollTop:local}, 1000);
+   
+});
+
 $('.contato').click(function(){
      $('html, body').animate({scrollTop:contato}, 1000);
-       return false;
-});
-
-
-$('.loja').click(function(){
-     $('html, body').animate({scrollTop:loja}, 1000);
       return false;
 });
-
-
-$('.site').click(function(){
-     $('html, body').animate({scrollTop:site}, 1000)
-     return false;
-});
-
-$('.home').click(function(){
-    
-    
-    $('html, body').animate({scrollTop:home}, 1000);
-            return false;
-      
-});
-
-
-
-
-
-
 
     
 
