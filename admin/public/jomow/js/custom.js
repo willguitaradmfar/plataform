2bfd9ead2c00b7d245f13314bab0b52826196055
William Lima <willguitaradmfar@gmail.com>
2014-01-29 07:45:17 -0500

    ajuste css

--- admin/public/jomow/js/custom.js
+++ admin/public/jomow/js/custom.js
@@ -7,16 +7,17 @@ $(document).ready(function(){
   
  
 });
-          $(document).mousemove(function(e){
-             TweenLite.to($('.siteconteudo'), 
-                .5, 
-                { css: 
-                    {
-                        'background-position':parseInt(event.pageX/8) + "px "+parseInt(event.pageY/12)+"px, "+parseInt(event.pageX/15)+"px "+parseInt(event.pageY/15)+"px, "+parseInt(event.pageX/30)+"px "+parseInt(event.pageY/30)+"px"
-                    }
-                });
-          });
-        });   
+
+$(document).mousemove(function(e){
+ TweenLite.to($('.siteconteudo'), 
+    .5, 
+    { css: 
+     {
+       'background-position':parseInt(event.pageX/8) + "px "+parseInt(event.pageY/12)+"px, "+parseInt(event.pageX/15)+"px "+parseInt(event.pageY/15)+"px, "+parseInt(event.pageX/30)+"px "+parseInt(event.pageY/30)+"px"
+    }
+       });
+      });
+});   
 
 //to declarando a posição da pagina
 var site = $("#site").offset().top;
