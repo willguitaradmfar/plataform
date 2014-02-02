74c9881f28467570aaeb1bf7786fe1f6b1d625fd
William <willguitaradmfar@gmail.com>
2014-02-02 13:36:15 -0200

    teste

---
+++ vaniadiarista.com.br/app.js
@@ -0,0 +1,70 @@
+
+var express = require('express'),
+        db = require('../schema/schema.js'),
+        path = require('path');                
+        _moduless = require('../module/_module.js');
+
+//---------------------------------------------------------------//
+moment = require('moment');
+//---------------------------------------------------------------//
+
+//---------------------------------------------------------------//
+var app = express();
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+//---------------------------------------------------------------//
+
+//---------------------------------------------------------------//
+config = require('./config.js');
+//---------------------------------------------------------------//
+
+//---------------------------------------------------------------//
+var redis = require("redis"),
+    redisClient = redis.createClient(config.redis.port, config.redis.host);
+    
+redisClient.auth(config.redis.password, function() {
+    console.log('Conectado no Redis HOST: '+config.redis.host);
+});
+  
+redisClient.on("error", function (err) {
+    console.log("Error " + err);
+});
+//---------------------------------------------------------------//
+var solr = require('solr-client');
+solrClient = solr.createClient({host: config.solr.host});
+//solrClient.basicAuth(config.solr.user, config.solr.password);
+solrClient.autoCommit = true;
+
+/---------------------------------------------------------------//
+
+app.configure(function() {
+        app.set('port', process.env.PORT || config.domain.port);
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'ejs');
+        app.use(express.favicon(__dirname+'/public/favicon/favicon.png'));
+        app.use(express.logger('dev'));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(express.cookieParser());
+        app.use(express.session({
+                secret: 'monkey'
+        }));
+        app.use(app.router);
+        app.use(require('stylus').middleware(__dirname + '/public'));   
+        app.use(express.bodyParser({
+                uploadDir: process.env.TMP
+        }));
+        app.use(express.static(path.join(__dirname, 'public')));
+});
+
+require('../module/apiDB.js')(app, config, db, require('../module/dao.js')(app, db, 'Produto'), redisClient, 'produto', 'vaniadiarista.com.br');
+require('../module/emailAPI')(app, config, redisClient, 'vaniadiarista.com.br');
+require('../module/queue.js')(config, redisClient, 'vaniadiarista.com.br');
+
+server.listen(app.get('port'), function() {
+    var msg = 'Projeto vaniadiarista.com.br esta executando na porta ' + app.get('port') + ' e IP '+process.env.IP +' em '+moment().format('MMMM Do YYYY, h:mm:ss a'); + '\n'
+        + JSON.stringify(config);
+        console.log(msg);
+        redisClient.rpush('vaniadiarista.com.br::::system::mail::administrator', JSON.stringify({text : msg, html : '<b>'+msg+'</b>'}));
+});
+
---
+++ vaniadiarista.com.br/public/js/resource.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+angular.module('app.resource', ["ngResource"])
+  
+   .factory('Email', function ($resource) {
+      return $resource('/mail', {}, {
+        enviar: {method:'POST'}
+      });
+  })  
+;
+
