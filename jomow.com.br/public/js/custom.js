61ab9061216c2e79a5100a0ce53fcaab322e6d19
William Lima <willguitaradmfar@gmail.com>
2014-02-02 11:06:16 -0500

    Jomow inclusao do fb-like

--- jomow.com.br/public/js/custom.js
+++ jomow.com.br/public/js/custom.js
@@ -1,15 +1,4 @@
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47695210-1', 'jomow.com.br');
-  ga('send', 'pageview');
-
-
-
-
 $(document).ready(function(){
   
   $("#btnmix").click(function() {
@@ -71,5 +60,4 @@ $('.bntsitetopo').click(function(){
      $('html, body').animate({scrollTop:siteconteudo}, 1000);
       return false;
 });
-
 
